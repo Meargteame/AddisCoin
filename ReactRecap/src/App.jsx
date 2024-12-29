c406fdc6419eb374d27e1460f5b1fb04a1391516
Meargteame <mearegteame99995555@gmail.com>
2024-12-29 19:58:53 +0300

    React Recap Study

---
+++ ReactRecap/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={function(){
+        setCount(count + 1)
+      }}>
+        Click Me {count}
+      </button>
+    </div>
+  );
+}
+
+export default App;
